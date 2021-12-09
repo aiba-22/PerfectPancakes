cd0203feee28de3e1456370f864b646a229e877f
aiba <jazzarubamu@gmail.com>
2021-12-09 17:09:09 +0900

    Fix:テキストエリアに改行を考慮する様に修正 & モデルのバリデーションを修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,6 @@ class Recipe < ApplicationRecord
   mount_uploader :image, RecipeImageUploader
   has_many :recipe_lists,dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :material, presence: true, length: { maximum: 300 }
 end
