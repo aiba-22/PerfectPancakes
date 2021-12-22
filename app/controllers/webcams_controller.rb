6e293e603452aea7ed1bb5091ecc0b0400076e75
aiba <jazzarubamu@gmail.com>
2021-12-22 18:47:06 +0900

    Fix:ログインしていない状態でもカメラ起動ページへの許可をしました

--- app/controllers/webcams_controller.rb
+++ app/controllers/webcams_controller.rb
@@ -1,4 +1,5 @@
 class WebcamsController < ApplicationController
+	skip_before_action :require_login, only: [:index]
     def index
         if current_user
           @favorite_baking = User.find(current_user.id).favorite_baking
