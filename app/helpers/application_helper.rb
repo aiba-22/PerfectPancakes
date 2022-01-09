206a6b1d00a8bbc92991fdf64ea1fe6c55c1a127
aiba <jazzarubamu@gmail.com>
2022-01-10 01:19:38 +0900

    Fix:最新をaddできていませんでしたので修正しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,16 +6,15 @@ module ApplicationHelper
 	def default_meta_tags
 		{
 			og: {
-				site_name: :site,
-				title: :title,
-				description: :description,
+				site_name: 'PerfectPancakes',
+				title: 'PerfectPancakes|パンケーキ作りをサポート',
+				description: 'あなたのパンケーキ作りをサポート、カメラで読み込んだパンケーキの映像から画像解析を行い、ひっくり返すタイミングを教えてくれます。焼き加減調調整やレシピ登録機能も搭載しています。',
 				type: 'website',
-				url: :canonical,
+				url: 'https://www.perfectpancakes.jp',
 				image: image_url('ogp.png'),
 				},
 				twitter: {
-				card: 'summary',
-				site: '@YuppyHappyToYou',
+				card: 'summary_large_image',
 			}
 		}
 	end
