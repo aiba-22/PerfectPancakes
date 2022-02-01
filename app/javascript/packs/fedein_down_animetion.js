47be3f6e226ba82f481cfa7967cb1c47c6112e37
aiba <jazzarubamu@gmail.com>
2022-02-02 00:25:13 +0900

    fix:文字をふわっと出すようにしました

---
+++ app/javascript/packs/fedein_down_animetion.js
@@ -0,0 +1,11 @@
+$('.animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('.animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
