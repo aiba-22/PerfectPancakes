78327b195db06ab3ef4e23e423625b10a000a273
aiba <jazzarubamu@gmail.com>
2022-02-09 09:11:05 +0900

    fix:コンフリクトが起きていたので修正

--- app/javascript/packs/fedein_down_animetion.js
+++ app/javascript/packs/fedein_down_animetion.js
@@ -3,9 +3,9 @@ $(window).scroll(function(){
     var windowHeight = $(window).height(),
         topWindow = $(window).scrollTop()
     $('.animation').each(function(){
-        var targetPosition = $(this).offset().top
-        if(topWindow > targetPosition - windowHeight + 60){
-            $(this).addClass('fadeInDown')
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight){
+            $(this).addClass('fadeInDown');
         }
     })
 })
